efcbf2d30a135acf3f1746d475edc07a40bd2568
Maxime Peuch <maxime.peuch@gmail.com>
2015-05-05 18:36:07 +0200

    Clean

--- src/com/mpeuck/firstG/desktop/DesktopLauncher.java
+++ src/com/mpeuck/firstG/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.mpeuck.firstG.MyFirstG;
 public class DesktopLauncher {
 	
 	public static void main (String[] arg) {
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyFirstG(), config);
 	}

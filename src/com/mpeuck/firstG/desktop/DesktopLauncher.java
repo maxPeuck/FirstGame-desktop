8591dfcd1e9fe2b70ae9c3e92b57f4b6ad0eae97
Maxime Peuch <maxime.peuch@gmail.com>
2015-05-05 18:35:05 +0200

    jfg

---
+++ src/com/mpeuck/firstG/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.mpeuck.firstG.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mpeuck.firstG.MyFirstG;
+
+public class DesktopLauncher {
+	
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyFirstG(), config);
+	}
+}
